181cf89460c31fbfed61571d0cb304736bc69c88
Dylan J. Gerrits <dyrits@outlook.com>
2023-12-22 11:02:33 +0100

    Tentative #9

--- api/services/nodemailer.js
+++ api/services/nodemailer.js
@@ -1,17 +1,19 @@
 const nodemailer = require("nodemailer");
 
 const transport = nodemailer.createTransport({
-    service: "gmail",
-    auth: {
-      user: process.env.MAIL_AUTH_USER,
-      pass: process.env.MAIL_AUTH_PASSWORD
-    }
+  service: "gmail",
+  auth: {
+    user: process.env.MAIL_AUTH_USER,
+    pass: process.env.MAIL_AUTH_PASSWORD
+  }
 });
 
 
 function sendMail(options) {
-    transport.sendMail(options);
-    console.log("Mail sent");
+  console.log(process.env.MAIL_AUTH_USER);
+  console.log(process.env.MAIL_AUTH_PASSWORD);
+  transport.sendMail(options);
+  console.log("Mail sent");
 }
 
 module.exports = sendMail;
